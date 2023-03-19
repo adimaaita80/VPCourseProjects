a26ec3c8444d49aefd52fb4211049b4909c50d73
Adi Maaita <adi.maaita80@gmail.com>
2023-03-19 09:52:15 +0300

    Lecture 4

---
+++ UnderstandTheCode/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandTheCode
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public Employee()
+        {
+            // This code will be executed whenever a new object is created
+        }
+
+        public string PrintEmployeeReport()
+        {
+            return "";
+        }
+    }
+}
---
+++ UnderstandTheCode/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnderstandTheCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Button button1 = new Button();
+            button1.Name = "Button1";
+            button1.Text = "Button1";
+            button1.Location = new Point(100, 100);
+            button1.Size = new Size(180, 52);
+
+            this.Controls.Add(button1);
+        }
+    }
+}
