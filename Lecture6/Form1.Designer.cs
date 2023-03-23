c41ec8a5048a65bf8801f7c389fc370b8f272444
Adi Maaita <adi.maaita80@gmail.com>
2023-03-23 10:26:33 +0300

    Lecture 6 - form load explanation

---
+++ Lecture6/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Lecture6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStudentId = new System.Windows.Forms.Label();
+            this.txtStudentId = new System.Windows.Forms.TextBox();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.lblDateOfBirth = new System.Windows.Forms.Label();
+            this.dtpDateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.cbxCountries = new System.Windows.Forms.ComboBox();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student information Form:";
+            // 
+            // lblStudentId
+            // 
+            this.lblStudentId.AutoSize = true;
+            this.lblStudentId.Location = new System.Drawing.Point(50, 110);
+            this.lblStudentId.Name = "lblStudentId";
+            this.lblStudentId.Size = new System.Drawing.Size(63, 13);
+            this.lblStudentId.TabIndex = 1;
+            this.lblStudentId.Text = "Student ID:";
+            // 
+            // txtStudentId
+            // 
+            this.txtStudentId.Enabled = false;
+            this.txtStudentId.Location = new System.Drawing.Point(119, 107);
+            this.txtStudentId.Name = "txtStudentId";
+            this.txtStudentId.Size = new System.Drawing.Size(145, 20);
+            this.txtStudentId.TabIndex = 2;
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(119, 158);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(339, 20);
+            this.txtStudentName.TabIndex = 4;
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.AutoSize = true;
+            this.lblStudentName.Location = new System.Drawing.Point(34, 161);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(79, 13);
+            this.lblStudentName.TabIndex = 3;
+            this.lblStudentName.Text = "Student Name:";
+            // 
+            // lblDateOfBirth
+            // 
+            this.lblDateOfBirth.AutoSize = true;
+            this.lblDateOfBirth.Location = new System.Drawing.Point(81, 218);
+            this.lblDateOfBirth.Name = "lblDateOfBirth";
+            this.lblDateOfBirth.Size = new System.Drawing.Size(32, 13);
+            this.lblDateOfBirth.TabIndex = 5;
+            this.lblDateOfBirth.Text = "DOB:";
+            // 
+            // dtpDateOfBirth
+            // 
+            this.dtpDateOfBirth.Location = new System.Drawing.Point(119, 212);
+            this.dtpDateOfBirth.Name = "dtpDateOfBirth";
+            this.dtpDateOfBirth.Size = new System.Drawing.Size(200, 20);
+            this.dtpDateOfBirth.TabIndex = 6;
+            // 
+            // cbxCountries
+            // 
+            this.cbxCountries.FormattingEnabled = true;
+            this.cbxCountries.Location = new System.Drawing.Point(119, 273);
+            this.cbxCountries.Name = "cbxCountries";
+            this.cbxCountries.Size = new System.Drawing.Size(200, 21);
+            this.cbxCountries.TabIndex = 7;
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.AutoSize = true;
+            this.lblCountry.Location = new System.Drawing.Point(56, 276);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(50, 13);
+            this.lblCountry.TabIndex = 8;
+            this.lblCountry.Text = "Country:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 43);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 479);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblCountry);
+            this.Controls.Add(this.cbxCountries);
+            this.Controls.Add(this.dtpDateOfBirth);
+            this.Controls.Add(this.lblDateOfBirth);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.lblStudentName);
+            this.Controls.Add(this.txtStudentId);
+            this.Controls.Add(this.lblStudentId);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Lecture 6";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStudentId;
+        private System.Windows.Forms.TextBox txtStudentId;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.Label lblDateOfBirth;
+        private System.Windows.Forms.DateTimePicker dtpDateOfBirth;
+        private System.Windows.Forms.ComboBox cbxCountries;
+        private System.Windows.Forms.Label lblCountry;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lecture6/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lecture6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtStudentId.Text = "AB0001";
+
+            cbxCountries.Items.Add("Jordan");
+            cbxCountries.Items.Add("Syria");
+            cbxCountries.Items.Add("Egypt");
+
+            dtpDateOfBirth.Value = DateTime.Now.AddYears(-18);
+        }
+    }
+}
